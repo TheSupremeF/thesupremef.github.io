d9e9069f85651477c7d9467dd91eb46cadefde69
Mustafa Burak Besler <70367976+TheSupremeF@users.noreply.github.com>
2025-12-10 15:39:41 +0300

    Fix fileName assignment syntax in events-io.js

--- Interaktif_Vaziyet/js/events/events-io.js
+++ Interaktif_Vaziyet/js/events/events-io.js
@@ -23,7 +23,7 @@
       const zip = new JSZip();
       const now = new Date();
       const pad = n => String(n).padStart(2, '0');
-      const fileName = 'site-plan-' + ns.state.projectInfo.contractorShort +'-'
+      const fileName = 'site-plan-' + ns.state.projectInfo.contractorShort +'-'+
         now.getFullYear() +
         pad(now.getMonth() + 1) +
         pad(now.getDate()) + '-' +
